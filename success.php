967f84cab9b0cba348b353e2d177985df6f3d841
lakshman7781 <lakshmanbhavanam009@gmail.com>
2024-02-14 11:03:51 +0530

    added push notification to xerox

--- success.php
+++ success.php
@@ -24,3 +24,27 @@ session_start();
 
 </body>
 </html>
+<?php
+require __DIR__ . '/vendor/autoload.php';
+use Twilio\Rest\Client;
+
+// Your Account SID and Auth Token from twilio.com/console
+// To set up environmental variables, see http://twil.io/secure
+$account_sid = "AC5c473de347005dcc885efd5affed455b";
+$auth_token = "42f7a08c3de957094471e07d43854837";
+// In production, these should be environment variables. E.g.:
+// $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
+
+// A Twilio number you own with SMS capabilities
+$twilio_number = "+16598883893";
+
+$client = new Client($account_sid, $auth_token);
+$client->messages->create(
+    // Where to send a text message (your cell phone?)
+    '+919494410554',
+    array(
+        'from' => $twilio_number,
+        'body' => 'I sent this message in under 10 minutes!'
+    )
+);
+?>
